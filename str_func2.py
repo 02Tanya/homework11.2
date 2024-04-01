66465f620b9f1b934ee5b81be10a59f5aab2dd28
TatyanaSh <tr3520@bk.ru>
2024-04-01 21:11:47 +0300

    Creation function for title and addition docstrings

--- str_func2.py
+++ str_func2.py
@@ -1,4 +1,10 @@
 def get_lowercase_phrase():
-
+    '''Переводит фразу в верхний регистр'''
     phrase = input('Введите фразу или предложение: ')
     return phrase.lower()
+
+
+def get_first_letter_up():
+    '''Повышает регистр первой буквы каждого слова'''
+    phrase = input('Введите фразу или предложение: ')
+    return phrase.title()
